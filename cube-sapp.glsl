692bbb2a14ba34a92bace5d501373740cd1ccf87
Ryan1729 <Ryan1729@gmail.com>
2022-03-06 00:06:00 -0700

    remove HandmadeMath.h at a significant loss of functionality
    I regenerated the cube-sapp.glsl.h file with an external copy of sokol-shdc
    
    At the moment, I think I'll avoid relying on sokol-shdc, since it has so many dependencies, and accept the limited compatibility. But it's nice to know that the option of wider compatibility is available if desired.

--- cube-sapp.glsl
+++ cube-sapp.glsl
@@ -1,17 +1,11 @@
-@ctype mat4 hmm_mat4
-
 @vs vs
-uniform vs_params {
-    mat4 mvp;
-};
-
 in vec4 position;
 in vec4 color0;
 
 out vec4 color;
 
 void main() {
-    gl_Position = mvp * position;
+    gl_Position = position;
     color = color0;
 }
 @end
