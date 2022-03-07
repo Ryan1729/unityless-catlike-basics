62b3803c25977f0d387fda423e538b5180041b7c
Ryan1729 <Ryan1729@gmail.com>
2022-03-07 00:06:00 -0700

    create bindings sys crate

---
+++ sokol-bindings-sys/src/sys.rs
@@ -0,0 +1,3 @@
+#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+mod gl_core_3_3;
+pub use gl_core_3_3::*;
---
+++ sokol-bindings-sys/wrapper.h
@@ -0,0 +1,5 @@
+#define SOKOL_IMPL
+#include "../third-party/sokol_gfx.h"
+#define SOKOL_NO_ENTRY
+#include "../third-party/sokol_app.h"
+#include "../third-party/sokol_glue.h"
