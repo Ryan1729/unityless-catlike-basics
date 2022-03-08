f1b06f451a4dc7f0fe5bc55d660b311bba72d019
Ryan1729 <Ryan1729@gmail.com>
2022-03-08 00:06:00 -0700

    fix linker error!

--- sokol-bindings-sys/build.rs
+++ sokol-bindings-sys/build.rs
@@ -5,6 +5,15 @@ fn main() {
     println!("cargo:rerun-if-changed=third-party/sokol_app.h");
     println!("cargo:rerun-if-changed=third-party/sokol_glue.h");
 
+    // TODO link to different platform specific libraries on other platforms
+    println!("cargo:rustc-link-lib=GL");
+    println!("cargo:rustc-link-lib=dl");
+    println!("cargo:rustc-link-lib=m");
+    println!("cargo:rustc-link-lib=X11");
+    println!("cargo:rustc-link-lib=asound");
+    println!("cargo:rustc-link-lib=Xi");
+    println!("cargo:rustc-link-lib=Xcursor");
+
     // TODO select this with a feature flag. Bindings generation will also need to
     // be updated.
     const BACKEND: &str = "SOKOL_GLCORE33";
@@ -12,5 +21,7 @@ fn main() {
     cc::Build::new()
         .file("wrapper.c")
         .define(BACKEND, None)
+        .flag("-pthread")
+        .include("../third-party")
         .compile("wrapper");
 }
