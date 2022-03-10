3a9668064cd789078e267c7a8ac49fc14484b009
Ryan1729 <Ryan1729@gmail.com>
2022-03-10 00:06:00 -0700

    update comments since sokol_gfx.h has userdata callbacks as well

--- sokol-bindings/src/sokol_bindings.rs
+++ sokol-bindings/src/sokol_bindings.rs
@@ -3,10 +3,12 @@
 #[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 pub use sokol_bindings_sys::*;
 
-/// This convenience macro creates a userdata callback for the sapp desc struct.
-/// Note that the paramter type indicated *must* be the same as the type of the 
-/// userdata field on the given sapp desc struct.
-/// passed to the `run` function.
+/// This convenience macro creates a userdata callback, for example for the sapp
+/// desc struct. Note that the paramter type indicated *must* be the same as the
+/// original type of the userdata field passed elsewhere, otherwise undefined
+/// behavior could occur. (Technically if they are the same size it might work,
+/// but if you want a union or whatever, there are better places to do that than
+/// here.)
 #[macro_export]
 macro_rules! cb_wrap_userdata {
     ($cb: ident : fn(&mut $type: ty)) => {{
