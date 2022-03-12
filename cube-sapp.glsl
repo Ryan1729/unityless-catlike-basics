3abd220d09647a1ae4d17dc78039c219410385e6
Ryan1729 <Ryan1729@gmail.com>
2022-03-12 00:06:00 -0700

    hardcode some matricies into the cut-down c example, to show the cube

--- cube-sapp.glsl
+++ cube-sapp.glsl
@@ -5,7 +5,30 @@ in vec4 color0;
 out vec4 color;
 
 void main() {
-    gl_Position = position;
+    float angle = 3.14f/3.0f;
+
+    mat4 rotate_x = mat4(
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, cos(angle), -sin(angle), 0.0f,
+        0.0f, sin(angle), cos(angle), 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    mat4 rotate_z = mat4(
+        cos(angle), -sin(angle), 0.0f, 0.0f,
+        sin(angle), cos(angle), 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    mat4 scale = mat4(
+        0.25f, 0.0f, 0.0f, 0.0f,
+        0.0f, 0.25f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.25f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f
+    );
+
+    gl_Position = (scale * rotate_x * rotate_z) * position;
     color = color0;
 }
 @end
