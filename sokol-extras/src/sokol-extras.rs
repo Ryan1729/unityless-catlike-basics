06742cab6c08719121df846567e27fef2f1cb706
Ryan1729 <Ryan1729@gmail.com>
2022-04-03 01:06:00 -0600

    create shaders module, keep the old path available

--- sokol-extras/src/sokol-extras.rs
+++ sokol-extras/src/sokol-extras.rs
@@ -1,5 +1,6 @@
 ///! A collection of things that seem like they would be useful in multiple places
 ///! but which don't fit in `sokol_bindings`.
 
-pub mod textured;
+pub mod shaders;
+pub use shaders::textured;
 pub mod white_image;
