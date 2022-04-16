09bc38251bc56ab0ea690d15294ef4052499ad4b
Ryan1729 <Ryan1729@gmail.com>
2022-04-16 01:06:00 -0600

    set default light_dir to more closelyy match second 2.2 screenshot

--- basics/game-objects-and-scripts/src/main.rs
+++ basics/game-objects-and-scripts/src/main.rs
@@ -135,7 +135,7 @@ fn init(state: &mut State) {
     state.eye.y = Radians(4.0);
     state.eye.z = Radians(4.375);
     state.eye.radius = 10.;
-    state.light_dir = vec3!(-1., 0., 1.);
+    state.light_dir = vec3!(1., 1./256., -1.);
     state.center = vec3!();
 }
 
